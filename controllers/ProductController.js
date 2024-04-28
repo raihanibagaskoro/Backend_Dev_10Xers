206f1e29c49bbe7c71421f6023f22df362b05b9a
raihanibagaskoro <raihanibagaskoro@gmail.com>
2024-04-28 20:20:20 +0700

    implementing getOne, getAll productController

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,10 +2,24 @@ const {Product} = require('../models')
 
 class ProductController {
     static async getAll(req, res, next){
-
+        try {
+            const products = await Product.findAll()
+            res.status(200).json(products)
+        } catch (error) {
+            next(error)
+        }
     }
     static async getOne(req, res, next){
-        
+        try {
+            const id = +req.params.id
+            const product = await Product.findByPk(id)
+            if (!product){
+                throw {message: 'Product not found'}
+            }
+            res.status(200).json(product)
+        } catch (error) {
+            next(error)
+        }
     }
     static async add(req, res, next){
 
@@ -14,7 +28,7 @@ class ProductController {
 
     }
     static async delete(req, res, next){
-
+        
     }
 }
 
--- router/index.js
+++ router/index.js
@@ -3,6 +3,9 @@ const ProductController = require('../controllers/ProductController')
 
 router.get('/')
 router.get('/product', ProductController.getAll)
+router.get('/product/:id', ProductController.getOne)
+router.put('/product/:id', ProductController.edit)
 router.post('/product', ProductController.add)
+router.delete('/product/:id', ProductController.delete)
 
 module.exports = router
