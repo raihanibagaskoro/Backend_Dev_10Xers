e9d151f2bc9521400c0ab26e1c6ec8ff6912eeb0
raihanibagaskoro <raihanibagaskoro@gmail.com>
2024-04-29 12:40:29 +0700

    implementing edit features

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -23,10 +23,41 @@ class ProductController {
         }
     }
     static async add(req, res, next){
-
+        try {
+            const { productName, price } = req.body
+            const newProduct = await Product.create({
+                productName, price
+            })
+            res.status(201).json(newProduct)
+        } catch (error) {
+            next(error)
+        }
     }
     static async edit(req, res, next){
-
+        try {
+            const id = +req.params.id
+            const { productName, price } = req.body
+            const product = await Product.findByPk(id)
+            if (!product) {
+                return res.status(404).json({
+                    error: 'Product not found'
+                })
+            }
+            await Product.update({
+                productName, price
+            },
+            {
+                where: {id}
+            })
+            res.status(200).json({
+                message: 'Product updated successfully'
+            })
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({
+                error: 'Internal server error'
+            })
+        }
     }
     static async search(req, res, next){
         try {
